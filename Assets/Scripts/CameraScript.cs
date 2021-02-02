6ca345968c21731fc9596852ee2498d9a0e767d2
Chika0IQ <mohdkhairin62@gmail.com>
2021-02-03 03:01:33 +0800

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    Vector3 offset = new Vector3(0f, 0.4f, 0.3f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera();
+    }
+
+    private void Camera()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, 0.1f);
+    }
+}
---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 300f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+    }
+}
